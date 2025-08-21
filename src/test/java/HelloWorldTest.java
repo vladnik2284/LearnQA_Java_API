6ca4a693da38e185a8b94f5620cca550f3c00a4b
Vladimir Nikolaev <vladnik2284@yandex.ru>
2025-08-21 22:24:37 +0400

    Test response 1

--- src/test/java/HelloWorldTest.java
+++ src/test/java/HelloWorldTest.java
@@ -1,9 +1,14 @@
+import io.restassured.RestAssured;
+import io.restassured.response.Response;
 import org.junit.jupiter.api.Test;
 
 public class HelloWorldTest {
 
     @Test
     public void testHelloWorld(){
-        System.out.println("Hello from Vladimir");
+        Response response = RestAssured
+                .get("https://playground.learnqa.ru/api/get_text")
+                .andReturn();
+        response.prettyPrint();
     }
 }
