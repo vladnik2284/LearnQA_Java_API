bc6c32475b7010322ff846be5f34fbc26b0e63c6
Vladimir Nikolaev <vladnik2284@yandex.ru>
2025-09-03 20:53:57 +0400

    Сделал все правки, вывел статус и результат

--- src/test/java/TokenTest.java
+++ src/test/java/TokenTest.java
@@ -1,11 +1,8 @@
 import io.restassured.RestAssured;
-import io.restassured.http.Headers;
 import io.restassured.path.json.JsonPath;
-import io.restassured.response.Response;
 import org.junit.jupiter.api.Test;
 
 import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
 
 
